d3e38939ef19d10a4e956014004cc0a6d1374cc0
Q3C-rypt <jacobantonious@rogers.com>
2024-10-11 13:49:45 -0400

    Main pages are done

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 redirect_url = 'http://127.0.0.1:5000/callback'
-scope = 'playlist-modify-public user-library-read playlist-read-private user-read-private user-read-email user-top-read'
+scope = 'playlist-modify-public playlist-modify-private user-library-read playlist-read-private user-read-private user-read-email user-top-read'
 
 cache_handler = FlaskSessionCacheHandler(session)
 sp_oauth = SpotifyOAuth(
@@ -28,7 +28,7 @@ sp_oauth = SpotifyOAuth(
         show_dialog=True
 )
 
-sp = Spotify(auth_manager=sp_oauth)
+sp = Spotify(auth_manager=sp_oauth, requests_timeout=15)
 
 @app.route('/')
 def home():
@@ -48,17 +48,17 @@ def callback():
     session['logged_in'] = True
     return redirect(url_for('home'))
 
-@app.route('/get_playlist')
-def get_playlist():
-    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
-        auth_url = sp_oauth.get_authorize_url()
-        return redirect(auth_url)
+# @app.route('/get_playlist')
+# def get_playlist():
+#     if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+#         auth_url = sp_oauth.get_authorize_url()
+#         return redirect(auth_url)
     
-    playlist = sp.current_user_playlists()
-    playlist_info = [(pl['name'], pl['external_urls']['spotify']) for pl in playlist['items']]
-    playlist_html = '<br>'.join([f'{name}: {url}' for name, url in playlist_info])
+#     playlist = sp.current_user_playlists()
+#     playlist_info = [(pl['name'], pl['external_urls']['spotify']) for pl in playlist['items']]
+#     playlist_html = '<br>'.join([f'{name}: {url}' for name, url in playlist_info])
 
-    return playlist_html
+#     return playlist_html
 
 @app.route('/get_toptracks')
 def get_toptracks():
@@ -78,9 +78,10 @@ def go_to_reco_play():
 @app.route('/make_reco_playlist', methods = ['POST'])
 def make_reco_playlist():
     # get energy, dancibility and genre from html
-    energy =request.form.get('energy')
-    dance = request.form.get('dancibility')
-    genre = request.form.get('genre')
+    play_name =request.form.get('play_name')
+    privacy = request.form.get('privacy') == 'True'
+    play_num = request.form.get('play_num')
+
 
     # get top tracks to recommend 
     top_tracks = sp.current_user_top_tracks(limit=50)
@@ -88,13 +89,18 @@ def make_reco_playlist():
 
     # get recommended
     # Spotify can only take in 5 seed tracks in one request
-    recommendations = sp.recommendations(seed_tracks=seed_tracks[:5], limit=20)
+    print(f"Play name: {play_name} Play NUm: {play_num} privacy: {privacy}")
 
-    # Create new playlist
+    recommendations = sp.recommendations(
+        seed_tracks=seed_tracks[:5],  # Seed tracks
+        limit=play_num
+    )
+
+    # # Create new playlist
     user_id = sp.current_user()['id']
-    playlist = sp.user_playlist_create(user_id, 'Personalized Recommended Playlist2', public=True) #add name and state
+    playlist = sp.user_playlist_create(user_id, play_name) #add name and state
     
-    # Add recommended to playlist
+    # # Add recommended to playlist
     track_ids = [track['id'] for track in recommendations['tracks']]
     sp.playlist_add_items(playlist['id'], track_ids)
 
